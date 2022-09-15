dadc418401f1cf01deaec33040465d7ea07aa4e4
1100012313 <1100012313@L815WD220008.edu.cbe.ab.ca>
2022-09-15 15:32:27 -0600

    3:32

--- Chapter 2/src/BinaryNumbers.java
+++ Chapter 2/src/BinaryNumbers.java
@@ -3,8 +3,27 @@ public class BinaryNumbers {
 
 	public static void main(String[] args) {
 
-
-
+		System.out.println("Decimal			Binary");
+		System.out.println("	  1				 1");
+		System.out.println("	  2				10");
+		System.out.println("	  3				11");
+		System.out.println("	  4			   100");
+		System.out.println("	  5			   101");
+		System.out.println("	  6			   110");
+		System.out.println("	  7			   111");
+		System.out.println("	  8			  1000");
+		System.out.println("	  9			  1001");
+		System.out.println("	 10			  1010");
+		System.out.println("	 11			  1011");
+		System.out.println("	 12			  1100");
+		System.out.println("	 13			  1101");
+		System.out.println("	 14			  1110");
+		System.out.println("	 15			  1111");
+		System.out.println("	 16			 10000");
+		System.out.println("	 17			 10001");
+		System.out.println("	 18			 10010");
+		System.out.println("	 19		     10011");
+		System.out.println("	 20			 10100");
 	}
 
 }
