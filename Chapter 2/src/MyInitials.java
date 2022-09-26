41a910b68dce690cee16e805d3891339e34e348e
1100012313 <1100012313@L815WD220008.edu.cbe.ab.ca>
2022-09-26 14:01:34 -0600

    2:00

---
+++ Chapter 2/src/MyInitials.java
@@ -0,0 +1,22 @@
+/*
+Author: Muhammad El Hafi    			Date:9/26/2022
+School: Chhs		
+Course: Computer Science 20
+ */
+public class MyInitials {
+	public static void main(String[] args) {
+		System.out.println("|\		/|   |-----");
+		System.out.println("| \	   / |   |");
+		System.out.println("|  \  /	 |   |----");
+		System.out.println("|	\/	 |   |");
+		System.out.println("|	 	 | . |----- .");
+		
+		/*
+		 |\		 /|   |-----
+		 | \    / |   |
+		 |  \  /  |   |----
+		 |   \/	  |   | 
+		 |	 	  | . |-----
+		 */
+	}
+}
---
+++ Chapter 3/src/Skillbuilding/RectanglePerimeter.java
@@ -0,0 +1,16 @@
+package Skillbuilding;
+
+public class RectanglePerimeter {
+
+	public static void main(String[] args) {
+		int Length = 13;
+		int Width = 4;
+		int XX = 34;
+		System.out.println("Find the perimeter.");
+		System.out.println("The Formula for perimeter is 2W + 2L.");
+	    System.out.println("The Length is "+Length+".");
+		System.out.println("The Width is "+Width+".");	
+		System.out.println("2"+Length+" + 2"+Width+"= "+XX);
+	}
+
+}
