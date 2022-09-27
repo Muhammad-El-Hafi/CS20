cbc7fdcc379c53128da2437a14937787b0615ca7
1100012313 <1100012313@L815WD220009.edu.cbe.ab.ca>
2022-09-27 15:33:15 -0600

    3:33

--- Chapter 3/src/Skillbuilding/distance.java
+++ Chapter 3/src/Skillbuilding/distance.java
@@ -7,6 +7,12 @@ public class distance {
 	public static void main(String[] args) {
 		
 		DecimalFormat dc = new DecimalFormat("0.0");
+		
+		double seg1 = 12.2;
+		double seg2 = 10.6;
+		double seg3 = 5.8;
+		double total = seg1 + seg2 + seg3;
+		System.out.println("The total distance of the race is " + dc.format(total) + "km" );
 
 	}
 
