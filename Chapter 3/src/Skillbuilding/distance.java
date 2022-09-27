a25afcc8612e65d8179be54c14789c56bb6a0b92
1100012313 <1100012313@L815WD220009.edu.cbe.ab.ca>
2022-09-27 15:24:55 -0600

    3:24PM

---
+++ Chapter 3/src/Skillbuilding/distance.java
@@ -0,0 +1,13 @@
+package Skillbuilding;
+
+import java.text.DecimalFormat;
+
+public class distance {
+
+	public static void main(String[] args) {
+		
+		DecimalFormat dc = new DecimalFormat("0.0");
+
+	}
+
+}
