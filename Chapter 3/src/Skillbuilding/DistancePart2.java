288320350465ff77c804ea79eb56b45cbb0c8de2
1100012313 <1100012313@L815WD220009.edu.cbe.ab.ca>
2022-09-28 13:53:45 -0600

    1:53

---
+++ Chapter 3/src/Skillbuilding/DistancePart2.java
@@ -0,0 +1,17 @@
+package Skillbuilding;
+
+public class DistancePart2 {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ Chapter 3/src/Skillbuilding/digits.java
@@ -0,0 +1,10 @@
+package Skillbuilding;
+
+public class digits {
+
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
